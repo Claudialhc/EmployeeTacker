788eb5bafe9fda837d9366399b4b33cc6a44bcda
Claudia Hernandez <claudiahdez13@gmail.com>
2021-01-20 22:30:50 -0800

    installed console.table

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 // import mysql, inquirer
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const cTable = require('console.table');
+//below to check if console.table works
+// console.table([
+//   {
+//     name: 'foo',
+//     age: 10
+//   }, {
+//     name: 'bar',
+//     age: 20
+//   }
+// ]);
